faf144a4d311044a64430a677d4b7cde6c55f5e4
Adnan Gondal <adnangondal1@hotmail.com>
2021-09-14 16:06:58 +0100

    defult + toggle value

--- src/components/CountriesSideBar/CountriesSideBar.js
+++ src/components/CountriesSideBar/CountriesSideBar.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CountriesSideBar.css";
 
 function CountriesSideBar() {
+  const [selected, setSelected] = useState(0);
+
+  const handleClick = (evt) => {
+    const id = evt.target.id;
+    const name = evt.target.value;
+    console.log(name);
+    setSelected(parseInt(id));
+  };
+
   return (
     <div className="sidebar">
       <h2> Select a Country</h2>
       <ul className="nav">
-        <li className="nav-elem">
-          <button>United Kingdom</button>
+        <li
+          onClick={handleClick}
+          className={`nav-elem ${selected === 0 ? "selected" : null}`}
+        >
+          <button id={0} value="United Kingdom">
+            United Kingdom
+          </button>
         </li>
-        <li className="nav-elem">
-          <button>Germany</button>
-        </li>
-        <li className="nav-elem">
-          <button>Pakistan</button>
-        </li>
-        <li className="nav-elem">
-          <button>China</button>
+        <li
+          onClick={handleClick}
+          className={`nav-elem ${selected === 1 ? "selected" : null}`}
+        >
+          <button id={1} value="Germany">
+            Germany
+          </button>
         </li>
       </ul>
     </div>
